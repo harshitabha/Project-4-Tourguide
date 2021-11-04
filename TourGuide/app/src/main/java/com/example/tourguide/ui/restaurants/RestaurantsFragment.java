f022f7ed8d7c70375eba65963a3c51f09e6176e9
Harshita Bhardwaj <honeyblue2604@gmail.com>
2021-11-03 23:26:04 -0700

    Restaurant layout and functionality
    Overall rest page should also be finished. Now we just need the values file error to go away.

--- TourGuide/app/src/main/java/com/example/tourguide/ui/restaurants/RestaurantsFragment.java
+++ TourGuide/app/src/main/java/com/example/tourguide/ui/restaurants/RestaurantsFragment.java
@@ -1,9 +1,12 @@
 package com.example.tourguide.ui.restaurants;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -15,10 +18,11 @@ import androidx.lifecycle.ViewModelProvider;
 import com.example.tourguide.R;
 import com.example.tourguide.databinding.FragmentRestaurantsBinding;
 
-public class RestaurantsFragment extends Fragment {
+public class restaurantsFragment extends Fragment implements View.OnClickListener{
 
-    private com.example.tourguide.ui.restaurants.RestaurantsViewModel restaurantsViewModel;
+    private RestaurantsViewModel restaurantsViewModel;
     private FragmentRestaurantsBinding binding;
+    private Button[] mapBtn;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -28,11 +32,61 @@ public class RestaurantsFragment extends Fragment {
         binding = FragmentRestaurantsBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textRestaurants;
+        //final TextView textView = binding.hotelTxt1;
+
+
+
+
         restaurantsViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
+                //initializing the buttons and setting it to the onclick listener
+                mapBtn = new Button[4];
+                String btnID;
+                for(int b = 0; b < mapBtn.length; b++)
+                {
+                    btnID = "address_rest" + b;
+                    int resourceID = getResources().getIdentifier(btnID, "id",
+                            getActivity().getPackageName());
+                    mapBtn[b] = (Button) getView().findViewById(resourceID);
+
+                    //what to do when the button is pressed
+                    mapBtn[b].setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            String btnID = v.getResources().getResourceEntryName(v.getId());
+                            int btnPointer = Integer.parseInt(btnID.substring(btnID.length() - 1)); //get the last character of the id (this will be a number)
+                            if(btnPointer==0)
+                            {
+                                Intent i = new Intent(Intent.ACTION_VIEW);
+                                i.setData(Uri.parse("geo:37.582700, 126.985098")); // coords for the 1st restaurant
+
+                                startActivity(i);
+                            }
+                            else if(btnPointer==1)
+                            {
+                                Intent i = new Intent(Intent.ACTION_VIEW);
+                                i.setData(Uri.parse("geo:37.551866, 126.988572")); // coords for the 2nd restaurant
+
+                                startActivity(i);
+                            }
+                            else if(btnPointer==2)
+                            {
+                                Intent i = new Intent(Intent.ACTION_VIEW);
+                                i.setData(Uri.parse("geo:37.551866, 126.988082")); // coords for the 3rd restaurant
+
+                                startActivity(i);
+                            }
+                            else
+                            {
+                                Intent i = new Intent(Intent.ACTION_VIEW);
+                                i.setData(Uri.parse("geo:37.570151, 126.999330")); // coords for the 4th restaurant
+
+                                startActivity(i);
+                            }
+                        }
+                    }); // onclick listener
+                }
             }
         });
         return root;
@@ -42,5 +96,10 @@ public class RestaurantsFragment extends Fragment {
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    @Override//to not have an error
+    public void onClick(View v) {
+
     }
 }
